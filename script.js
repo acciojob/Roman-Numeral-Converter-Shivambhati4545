9a52999650694e6a0bdb34939a0815e349c4de15
Shivambhati4545 <shivambhatiji@gmail.com>
2026-08-14 17:04:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
- function convertToRoman(num) {
-  const lookup = [
-    ['M', 1000], ['CM', 900], ['D', 500], ['CD', 400],
-    ['C', 100], ['XC', 90], ['L', 50], ['XL', 40],
-    ['X', 10], ['IX', 9], ['V', 5], ['IV', 4], ['I', 1]
+function convertToRoman(num) {
+  const obj = [
+    ['M', 1000],
+    ['CM', 900],
+    ['D', 500],
+    ['CD', 400],
+    ['C', 100],
+    ['XC', 90],
+    ['L', 50],
+    ['XL', 40],
+    ['X', 10],
+    ['IX', 9],
+    ['V', 5],
+    ['IV', 4],
+    ['I', 1]
   ];
   let result = '';
-  for (const [symbol, value] of lookup) {
+  for (let i = 0; i < obj.length; i++) {
+    const [symbol, value] = obj[i];
     while (num >= value) {
       result += symbol;
       num -= value;
     }
   }
   return result;
-}
 }
 console.log(convertToRoman(14));
 // do not edit below this line
